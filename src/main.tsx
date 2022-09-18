bf74df6728ef5c5a25c6ca76b4f14b8f8fe7f50c
davidadediji <davidadediji@gmail.com>
2022-09-18 22:47:15 +0100

    feat:add styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import '@fontsource/raleway/400.css';
+import '@fontsource/open-sans/700.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './utils/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<ChakraProvider theme={theme}>
+			<App />
+		</ChakraProvider>
+	</React.StrictMode>
+);
